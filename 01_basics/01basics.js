347c8a535aa0bf279c83c5d3b277a3d566511780
kenil_patel <120002567+kenil200@users.noreply.github.com>
2023-11-02 12:29:40 +0000

    variables

---
+++ 01_basics/01basics.js
@@ -0,0 +1,14 @@
+const accountid = 12345
+let accountpassword ="kenil@123"
+var accountemail="kenil@gmail.com"
+accountcity="rajkot"
+
+
+
+ 
+accountpassword ="kenil@456789"
+accountcity="surat"
+accountemail="kenil200@gmail.com"
+
+console.table([accountpassword,accountcity,accountemail,]);
+
---
+++ 01_basics/02datatype.js
@@ -0,0 +1,17 @@
+// 
+"use strict"; // treat all js code as new version
+
+// alert("hello kenil"); we are using nodejs, not browser
+
+// number 2 to power 53 
+// string =>  ""
+// boolean => true false
+// null  => standalone value 
+// undefine value =>
+// symbol => unique
+
+//Object value null
+
+let kenil;
+
+console.log(typeof "");
