02ceea2ae1c8672a73ca503cf4933da9c2721829
kenil_patel <120002567+kenil200@users.noreply.github.com>
2023-11-03 07:19:46 +0000

    conversetion.operation

---
+++ 01_basics/03conversetion.operation.js
@@ -0,0 +1,37 @@
+// let number = 1
+// let numberconversion = Boolean(number)
+// console.log(numberconversion)
+// console.log(typeof numberconversion);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true  => 1 fales => 0
+// 
+
+// ********************************operation************************************
+     
+//  let value = 3
+//  let negativevale = -value
+//  console.log(negativevale )
+
+// console.log(1+1);
+// console.log(1*1);
+// console.log(1/1);
+// console.log(1-1);
+// console.log(1%1);
+// console.log(1**1);
+
+let str1 ="hello"
+let str2 =" kenil"
+let str3 = str1 + str2
+console.log(str3);
+console.log(str1);
+console.log(str2);
+
+
+console.log("1"+1);
+console.log(1+"1");
+console.log("1"+2+2)
+console.log(1+1+2+"2");
+
