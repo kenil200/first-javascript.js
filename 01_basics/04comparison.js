0e3430cbbb6d7c3958a738c875963f1f2594f3c5
kenil_patel <120002567+kenil200@users.noreply.github.com>
2023-11-03 10:39:07 +0000

    conversetion.operation

---
+++ 01_basics/04comparison.js
@@ -0,0 +1,19 @@
+ console.log(null > 0);
+ console.log(null == 0);
+ console.log(null >= 0);
+
+ console.log(undefined == 0);
+ console.log(undefined > 0);
+ console.log(null < 0 );
+
+ console.log(1 > 0);
+console.log(10 >= 9);
+console.log(0 < 1);
+ console.log(2 == 2);
+ console.log(10 != 12);
+
+console.log("45" === "45");
+
+ console.log("2" >1);
+ console.log(15 < "10");
+
