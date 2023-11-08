18af533e5b1ab71a05943b681c72eb701c8cec4f
kenil_patel <120002567+kenil200@users.noreply.github.com>
2023-11-08 11:38:38 +0000

    datatypes

---
+++ 01_basics/05datatypesummary.js
@@ -0,0 +1,122 @@
+
+// const score = 100
+// const  scorevalue = 100.3
+
+// console.log("score_1");
+
+// const id = Symbol ('12345');
+
+// const  anotherid = Symbol ('456789');
+
+// console.log(id == anotherid);
+
+// const bignumber = 123456789123456789n
+
+// // const heros =["shktiman","naagraj","doga"]
+// // let myObject ={
+// //     name:"kenil",
+// //     age:"20",
+
+
+// const myFuntion = function(){
+    
+//  console.log("hello kenil");
+
+// }
+
+// // stack (primitivtype) heap(non-primitive)
+
+// let myname = "kakadiya"
+
+// let mycheanulname ="k k kakadiya"
+// mycheanulname ="kenil kakadiya"
+// console.log(myname);
+
+
+// let = user1={
+//     gmail:"user@gmail.com",
+//     upi:"user@byt"
+// }
+// console.log(user1);
+
+// let twouser2={
+//    gmail: "kenil@gmail.come",
+//    upi:"kenil@123"
+
+// }
+// console.log(twouser2);
+
+// twouser2.gmail = "kenil123@gmail.come"
+// console.log(user1.gmail);
+// console.log(twouser2.gmail);
+    
+
+    // const name = "kenil"
+    // const number ="10"
+    //  console.log(`${name}${number}`)
+    //  console.log(name + number);
+
+    //  const game = new String ('kenil');
+    //  console.log(game);
+   
+    // const name ="kenil"
+    // const number="20"
+    // console.log(`${name}${number}`);
+
+
+                  //String
+
+
+    //  const game  = new  String(`kakadiya`);
+    //  console.log("name,number");
+
+    // console.log(game[0]);
+    // console.log(game.__proto__);
+
+     //console.log(game.length);
+    // console.log(game.toUpperCase());
+       // console.log(game.charAt(5));
+    //   console.log(game.indexOf('d'));
+
+    // const newstring = game.substring(0,3);
+    // console.log(newstring);
+
+    // const anotherstring = game.slice(0,2)
+    // console.log(anotherstring);
+
+    // const newstring =game.substring(0,3)
+    // console.log(newstring);
+
+    // const newsstring = game.indexOf(1,2)
+    // console.log(newsstring)
+
+    // const kainai = game.slice(0,5)    
+    // console.log(kainai);
+
+
+    // const newstringone = "   kenil   "
+    // console.log(newstringone);
+    // console.log(newstringone.trim());
+
+    // const url ="http://kenil.com/kenil%20coundary"
+
+    // console.log(url.replace('20','-'));
+
+    // const url_1 ="https://kenil.com/kenil%20coundary"
+    // console.log(url_1.replace('20','-'))
+
+    // console.log(url_1.includes('sunder'));
+
+    const str ="my name is kakadiya kenil "
+     const words = str.split ('');
+    console.log(words[10]);
+
+    const chars = str.split ('');
+    console.log(chars[8]);
+
+    const strcopy =str.split('')
+    console.log(strcopy)
+
+
+    
+
