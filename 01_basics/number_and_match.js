23ee8c3a42f987bcc68cc3df19abf84fb0d182a9
kenil_patel <120002567+kenil200@users.noreply.github.com>
2023-11-09 08:19:39 +0000

    number and match

---
+++ 01_basics/number_and_match.js
@@ -0,0 +1,14 @@
+
+// const number = 400
+// console.log(number)
+
+const balance = new Number ('100')
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(10));
+
+const numberbalance = 123.899
+console.log(numberbalance.toPrecision(4));
+
+const hunreds = 1000000
+console.log(hunreds.toLocaleString('en-In'))
