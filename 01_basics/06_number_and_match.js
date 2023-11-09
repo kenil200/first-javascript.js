d7031f83057d7acb6d52d80421370139954b2367
kenil_patel <120002567+kenil200@users.noreply.github.com>
2023-11-09 10:11:09 +0000

    number and math

---
+++ 01_basics/06_number_and_match.js
@@ -0,0 +1,27 @@
+
+// const number = 400
+// console.log(number)
+
+// const balance = new Number ('100')
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(10));
+
+//const numberbalance = 123.899
+//console.log(numberbalance.toPrecision(4));
+
+//const hunreds = 1000000
+//console.log(hunreds.toLocaleString('en-In'))
+
+
+// console.log(Math.abs(1));
+// console.log(Math.round(4.1));
+// console.log(Math.ceil(4.4))
+// console.log(Math.floor(2.1));
+// console.log(Math.min(4,5,6,7,8));
+// console.log(Math.max(4,5,6,7,8));
+
+console.log(Math.floor(Math.random(1)*12)+1);
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random ()*(max-min+1))+min);
